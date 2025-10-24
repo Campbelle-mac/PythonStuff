22efcec08a0e7eefde93f450ace9c06ddd34a074
Campbelle-mac <campbelle@macstudent.com.au>
2025-10-24 02:59:37 +0000

    Problem 1.2

---
+++ 1-Conditionals/extensions.py
@@ -0,0 +1,16 @@
+x = input("File name: ")
+
+if "gif" in x:
+    print("image/gif")
+elif "png" in x:
+    print("image/png")
+elif "jpg" in x:
+    print("image/jpeg")
+elif "jpeg" in x:
+    print("image/jpeg")
+elif "pdf" in x:
+    print("application/pdf")
+elif "txt" in x:
+    print("application.txt")
+elif "zip" in x:
+    print("application.zip")

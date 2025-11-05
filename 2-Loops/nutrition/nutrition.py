ebbbe2080172e3936b8d2b9973b9ab9515942221
Campbelle-mac <campbelle@macstudent.com.au>
2025-11-05 21:59:17 +0000

    Problem 2.5 extension

--- 2-Loops/nutrition/nutrition.py
+++ 2-Loops/nutrition/nutrition.py
@@ -1,18 +1,29 @@
 food = {
-    "apple": "Calories: 130",
-    "banana": "Calories: 89",
-    "strawberry": "Calories: 45",
-    "mango": "Calories: 60",
+    "Apple": "130 Calories",
+    "Banana": "89 Calories",
+    "Strawberry": "45 Calories",
+    "Mango": "60 Calories",
 }
 
 
 def main():
-    item = input("Item: ")
+    item = input("Items: ")
+    items = item.split()
+    for key in items:
+        key = key.title()
+        if key in food:
+            print(key, "is", food[key])
+        else:
+            print(key, "not found")
+
+
+"""def main():                 - Original problem 
+    items = input("Items: ")
     key = item.lower()
     if key in food:
         print(food[key])
     else:
-        print("Not found")
+        print("Not found")"""
 
 
 main()
